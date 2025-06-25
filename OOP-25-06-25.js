3f52b49490169e12d4bad3982e993fe1ea7cc644
Moti7950 <shalom7950@gmail.com>
2025-06-25 15:45:32 +0300

    Q1,2,3

---
+++ OOP-25-06-25.js
@@ -0,0 +1,42 @@
+// ============1==========
+// class Phone 
+// {
+//     constructor (brand,model)
+//     {
+//         this.brand = brand;
+//         this.model = model;
+//         this.details = function(){console.log(`Phone: ${brand} ${model}`);
+//         }
+//     }
+// }
+// const motiPhone = new Phone("iphone","16 Pro 😁")
+// motiPhone.details()
+
+// ============2==========
+// class Rectangle
+// {
+//     constructor (width ,height)
+//     {
+//         this.width = width;
+//         this.height = height;
+//         this.area = function(){return `width = ${width} \nheight = ${height}\narea = ${width * height}`};
+//     }
+// }
+// const coulcRectangle =new Rectangle(5,4);
+// console.log(coulcRectangle.area());
+
+// ============3==========
+// class BankAccount
+// {
+//     constructor(owner ,balance)
+//     {
+//         this.owner =owner;
+//         this.deposit = function(AmountOfMoneyToAdd){return balance + AmountOfMoneyToAdd, this.checkBalance()};
+//         this.withdraw = function(AmountOfMoneyToSub){return balance - AmountOfMoneyToSub, this.checkBalance()};
+//         this.checkBalance = function(){return `${owner} have ${balance} $`};
+//     }
+// }
+
+// const motiBankAccount = new BankAccount("Moti",1000)
+// console.log(motiBankAccount.checkBalance());
+
